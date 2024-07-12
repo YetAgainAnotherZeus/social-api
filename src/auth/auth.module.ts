601020beb94555876785d3d70b36019e3ee6c1ff
Pierre <p.couturier3400@gmail.com>
2024-07-12 20:26:48 +0200

    fix: update style with new prettier config

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,17 +1,17 @@
-import { Module } from '@nestjs/common';
-import { AuthController } from './auth.controller';
-import { AuthService } from './auth.service';
-import { JwtModule } from '@nestjs/jwt';
+import { Module } from "@nestjs/common";
+import { AuthController } from "./auth.controller";
+import { AuthService } from "./auth.service";
+import { JwtModule } from "@nestjs/jwt";
 
 @Module({
-  imports: [
-    JwtModule.register({
-      global: true,
-      secret: process.env.JWT_SECRET || 'secret',
-      signOptions: { expiresIn: '60s' },
-    }),
-  ],
-  controllers: [AuthController],
-  providers: [AuthService],
+    imports: [
+        JwtModule.register({
+            global: true,
+            secret: process.env.JWT_SECRET || "secret",
+            signOptions: { expiresIn: "60s" },
+        }),
+    ],
+    controllers: [AuthController],
+    providers: [AuthService],
 })
 export class AuthModule {}
